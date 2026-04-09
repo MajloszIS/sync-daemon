0d9daf0aca16665137e2cad15ea782306253680f
MajloszIS <Psp13msz@gmail.com>
2026-04-09 23:07:26 +0200

    Step 1: validate arguments and check if paths are directories

--- demon.c
+++ demon.c
@@ -1,54 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <syslog.h>
-
-void stworz_demona() {
-    pid_t pid;
-
-    // Krok 1: pierwszy fork
-    pid = fork();
-    if (pid < 0) exit(EXIT_FAILURE); // błąd
-    if (pid > 0) exit(EXIT_SUCCESS); // rodzic kończy
-
-    // Krok 2: nowa sesja
-    if (setsid() < 0) exit(EXIT_FAILURE);
-
-    // Krok 3: drugi fork
-    pid = fork();
-    if (pid < 0) exit(EXIT_FAILURE);
-    if (pid > 0) exit(EXIT_SUCCESS);
-
-    // Krok 4: katalog roboczy
-    chdir("/");
-
-    // Krok 5: zamknij stdin/stdout/stderr
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
-}
 
 int main(int argc, char *argv[]) {
+    // Sprawdzenie liczby argumentów
     if (argc < 3) {
         fprintf(stderr, "Użycie: %s <src> <dst>\n", argv[0]);
         return 1;
     }
 
-    stworz_demona();
+    //Pobranie argumentów
+    char *src = argv[1];
+    char *dst = argv[2];
 
-    // Od tego miejsca jesteśmy demonem
-    openlog("sync-daemon", LOG_PID, LOG_DAEMON);
-    syslog(LOG_INFO, "Demon uruchomiony, src=%s dst=%s", argv[1], argv[2]);
+    // stat - przechowuje infmacje o pliku st
+    struct stat st;
 
-    // Główna pętla
-    while (1) {
-        syslog(LOG_INFO, "Demon śpi...");
-        sleep(10); // na razie 10 sekund żeby łatwiej testować
-        syslog(LOG_INFO, "Demon się obudził");
+    // Sprawdzenie czy src istnieje lub błąd
+    if (stat(src, &st) != 0) {
+        fprintf(stderr, "Błąd: nie można odczytać ścieżki %s\n", src);
+        return 1;
+    }
+    // makro S_ISDIR sprawdza czy to katalog
+    if (!S_ISDIR(st.st_mode)) {
+        fprintf(stderr, "Błąd: %s nie jest katalogiem\n", src);
+        return 1;
     }
 
-    closelog();
+    // Sprawdzenie czy dst istnieje lub błąd
+    if (stat(dst, &st) != 0) {
+        fprintf(stderr, "Błąd: nie można odczytać ścieżki %s\n", dst);
+        return 1;
+    }
+    // makro S_ISDIR sprawdza czy to katalog
+    if (!S_ISDIR(st.st_mode)) {
+        fprintf(stderr, "Błąd: %s nie jest katalogiem\n", dst);
+        return 1;
+    }
+
+    printf("OK: src=%s dst=%s\n", src, dst);
     return 0;
 }
