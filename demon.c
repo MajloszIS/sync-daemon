d21101cce3fc457db32b6c81d5e85cd71b8ae056
MajloszIS <Psp13msz@gmail.com>
2026-04-10 00:02:35 +0200

    create function create_daemon which start daemon

--- demon.c
+++ demon.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/stat.h>
+#include <syslog.h>
+
+void create_daemon() {
+    pid_t pid; // zmienna dla PID procesu
+
+    // Kopia procsesu i zakonczenie procesu rodzica lub bledu
+    pid = fork();
+    if (pid < 0) exit(EXIT_FAILURE);
+    if (pid > 0) exit(EXIT_SUCCESS);
+
+    // Dziecko tworzy nową sesję - odłącza się od terminala
+    if (setsid() < 0) exit(EXIT_FAILURE);
+
+    // Zabezpieczenie - proces nie może z powrotem przejąć żadnego terminala - sesja zostaje bez lidera
+    pid = fork();
+    if (pid < 0) exit(EXIT_FAILURE);
+    if (pid > 0) exit(EXIT_SUCCESS);
+
+    // Zmiana katalogu roboczego na /
+    chdir("/");
+
+    // zamknięcie zbędnych deskryptorów stdin/stdout/stderr
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+}
 
 int main(int argc, char *argv[]) {
     // Sprawdzenie liczby argumentów
@@ -38,6 +65,21 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    printf("OK: src=%s dst=%s\n", src, dst);
+    // Po walidacji - uruchamiamy demona
+    create_daemon();
+
+    // konfiguracja sysloga i logowanie informacji o uruchomieniu demona
+    openlog("sync-daemon", LOG_PID, LOG_DAEMON);
+    syslog(LOG_INFO, "Demon uruchomiony src=%s dst=%s", src, dst);
+
+    // Główna pętla
+    while (1) {
+        syslog(LOG_INFO, "Demon spi...");
+        sleep(10);
+        syslog(LOG_INFO, "Demon sie obudzil");
+        // tutaj będzie synchronizacja
+    }
+
+    closelog();
     return 0;
 }
